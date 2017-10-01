fdd62749a2e24f1105c462b527171734ce121fbf
VidhyaBatmaradjy <32403091+VidhyaBatmaradjy@users.noreply.github.com>
2017-10-01 20:42:03 +0100

    Update HashReducer.java

--- code/HashTweet/src/HashReducer.java
+++ code/HashTweet/src/HashReducer.java
@@ -5,41 +5,20 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-
 public class HashReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
 
      private IntWritable result = new IntWritable();
      
      public static float round(float value, int scale) {
     	    return (float)(Math.round(value * Math.pow(10, scale)) / Math.pow(10, scale));
-    	}
-	
-	 public void reduce(Text key, Iterable<IntWritable> values, Context context)
-
-              throws IOException, InterruptedException {
-
-       int sum =  0;
-     	
-
-        for (IntWritable value : values) {
-
-            //complete code here
-            
-            sum+= value.get();
-          
-            
-
-        }
-        
-         
-      result.set(sum);
-
-      context.write(key,result);
-       
-
-    }
-
-
-
+     }
+     
+     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+	   int sum =  0;
+           for (IntWritable value : values) {
+                sum+= value.get();
+           }
+           result.set(sum);
+           context.write(key,result);
+     }
 }
