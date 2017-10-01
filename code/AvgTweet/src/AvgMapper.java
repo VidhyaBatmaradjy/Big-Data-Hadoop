376cade4016511921d48af2d728b93a3d1b99bc0
VidhyaBatmaradjy <32403091+VidhyaBatmaradjy@users.noreply.github.com>
2017-10-01 19:55:34 +0100

    Update AvgMapper.java

--- code/AvgTweet/src/AvgMapper.java
+++ code/AvgTweet/src/AvgMapper.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-//import java.util.StringTokenizer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,28 +6,20 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 
 public class AvgMapper extends Mapper<LongWritable, Text, Text, IntWritable> { 
-  //  private final IntWritable one = new IntWritable(1);
-   
+  
     private IntWritable data = new IntWritable();
-    
     private Text avg = new Text("Tweet Averaqe");
-    
     private Text tweet1 = new Text();
     
     public void map(LongWritable key, Text line, Context context) throws IOException, InterruptedException {
      
-    	    	
-    String[] fields = line.toString().split(";");
-   
-    if(fields.length == 4)
-     {
+    	  	String[] fields = line.toString().split(";");
+          if(fields.length == 4)
+             {
     	
-    	data.set(fields[2].length());
- 	
- 	context.write(avg, data);
-     
-     
-     }
+    	          data.set(fields[2].length());
+ 	              context.write(avg, data);
+             }
         
-           }
+    }
 }
